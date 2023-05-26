8cf5ad956ab5c6d549de7c21a29b8d69b4abdf6a
davidryan-316 <l00177808@atu.ie>
2023-05-26 11:23:49 +0100

    Added BodyPart test

---
+++ UnitTests/CalorieCalculatorTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WODWarriors.ViewModel;
+using Xunit;
+
+namespace UnitTests
+{
+    public class CalorieCalculatorViewModelTest
+    {
+        [Fact]
+        public void CalorieCalculatorViewModel_Constructor_Properties()
+        {
+            // Arrange
+            var viewModel = new CalorieCalculatorViewModel();
+
+            // Assert
+            // Verify that the properties are initialized to their expected values
+            Assert.Null(viewModel.Age);
+            Assert.Null(viewModel.Weight);
+            Assert.Null(viewModel.Height);
+            Assert.Null(viewModel.CaloriesNeeded);
+        }
+
+        //[Fact]
+        //public async Task Calculate_ShouldSetCaloriesNeededAsync()
+        //{
+        //    // Arrange
+        //    var viewModel = new CalorieCalculatorViewModel();
+        //    viewModel.Age = 30;
+        //    viewModel.Weight = 70;
+        //    viewModel.Height = 170;
+        //    viewModel.Gender = "Male";
+        //    viewModel.ActivityLevel = "Moderately Active";
+
+        //    // Act
+        //    await viewModel.Calculate();
+
+        //    // Assert
+        //    double expectedCalories = 1.55 * (66.5 + (13.75 * viewModel.Weight.Value) + (5.003 * viewModel.Height.Value) - (6.75 * viewModel.Age.Value));
+        //    Assert.Equal(expectedCalories, viewModel.CaloriesNeeded.Value, 1);
+
+        //}
+    }
+}
---
+++ UnitTests/MovementTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WODWarriors.Model;
+
+namespace UnitTests
+{
+    public class MovementTest
+    {
+        [Fact]
+        public void Movement_Properties_Should_Be_Set_Correctly()
+        {
+            // Arrange
+            string name = "Squat";
+            string image = "barbell_full_squat.jpg";
+            string title = "Squat Exercise";
+            string description = "Target the quads and core stability";
+
+            // Act
+            var movement = new Movement
+            {
+                Name = name,
+                Image = image,
+                Title = title,
+                Description = description
+            };
+
+            // Assert
+            Assert.Equal(name, movement.Name);
+            Assert.Equal(image, movement.Image);
+            Assert.Equal(title, movement.Title);
+            Assert.Equal(description, movement.Description);
+        }
+    }
+}
+
